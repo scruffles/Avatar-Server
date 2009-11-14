f4fd8fac29597c4c0bec77084be19e5ec58b6361
scruffles <Bryan.Young@scruffles.net>
2009-11-14 10:07:27 -0600

    init checkin - working as a jetty web app

---
+++ src/main/java/net/scruffles/avatar/Main.java
@@ -0,0 +1,8 @@
+package net.scruffles.avatar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("running still");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+hello world <%net.scruffles.avatar.Main.main(null);%>
+</body>
+</html>
