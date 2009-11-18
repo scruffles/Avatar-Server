629dc1fdffe99ecdf61b87f1330535e43303d74c
scruffles <Bryan.Young@scruffles.net>
2009-11-17 22:29:44 -0600

    bluring before resizing for crisp thumbnails

--- src/main/java/net/scruffles/avatar/ImageUtil.java
+++ src/main/java/net/scruffles/avatar/ImageUtil.java
@@ -2,12 +2,26 @@ package net.scruffles.avatar;
 
 import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
-import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageUtil {
 
-    public static BufferedImage createResizedCopy(Image originalImage, int scaledWidth, int scaledHeight,
+    public static BufferedImage createResizedCopy(BufferedImage image, int scaledWidth, int scaledHeight,
+            boolean preserveAlpha)
+    {
+        // todo consider replacing this with Java Advanced Imaging: https://jai.dev.java.net/
+        image = scaleImage(image, 512, 512, preserveAlpha);
+        image = blurImage(image);
+        return scaleImage(image, scaledWidth, scaledHeight, preserveAlpha);
+    }
+
+    private static BufferedImage scaleImage(BufferedImage originalImage, int scaledWidth, int scaledHeight,
             boolean preserveAlpha)
     {
         int imageType = preserveAlpha ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
@@ -19,5 +33,27 @@ public class ImageUtil {
         g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
         g.dispose();
         return scaledBI;
+    }
+
+    public static BufferedImage blurImage(BufferedImage image) {
+        float ninth = 1.0f / 9.0f;
+        float[] blurKernel = {
+                ninth, ninth, ninth,
+                ninth, ninth, ninth,
+                ninth, ninth, ninth
+        };
+
+        Map map = new HashMap();
+
+        map.put(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        map.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+        map.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        RenderingHints hints = new RenderingHints(map);
+        BufferedImageOp op = new ConvolveOp(new Kernel(3, 3, blurKernel), ConvolveOp.EDGE_NO_OP, hints);
+        return op.filter(image, null);
     }
 }
